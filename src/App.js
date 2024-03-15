622b5915350cf37d84528977206fac75cc85cd2c
PremRathan2003 <premrathanaddanki7736@gmail.com>
2024-03-15 13:35:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React from 'react'
+import ReactPlayer from 'react-player'
 import './App.css';
+import { FaWhatsapp } from "react-icons/fa";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row">
+        <div>
+          <h1 className='heading'>Mobishaala</h1>
+          <p className='para'>Create your <span className='in-color'>Digital Classroom</span> in 5 mins for <span className='in-color'>FREE</span> & connect to 1+ Million Students.</p>
+          <ul class="circle-list">
+            <li>No technical knowledge required.</li>
+            <li>Quickly setup live class, tests, notes, ebooks and more.</li>
+            <li>Reach out to millions of students anywhere, anytime.</li>
+          </ul>
+          <div className='btn-container'>
+          <div>
+          <p>Institute/Teacher</p>
+          <button type="button" className='get-btn'>Get Started</button>
+          </div>
+          <div>
+          <p>Student</p>
+          <button type="button" className='get-btn'>Download App</button>
+          </div>
+         <button type='button' className='talk-btn'><FaWhatsapp size={25} className='icon'/> Talk to Us</button>
+        </div>
+        </div>
+        <div className='video-container'>
+        <ReactPlayer url='https://youtu.be/jg5d59ligW4?si=xb0yCsryyteJeEu4' controls={true}  width="100%" height="100%"/>
+        <button type="button" className='get-btn buy-btn'>Buy</button>
+
+        </div>
+      </div>
     </div>
   );
 }
